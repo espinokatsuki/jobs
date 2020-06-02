227898ce2b6aab981974fa1d0b2ca8e45ba609ab
Espino <edespinom@gmail.com>
2020-06-02 00:51:40 -0500

    Basic model Vacant object

---
+++ src/main/java/io/maya/jobs/model/Vacant.java
@@ -0,0 +1,61 @@
+package io.maya.jobs.model;
+
+public class Vacant {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private Double salary;
+    private Boolean active;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Vacant{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", salary=" + salary +
+                ", active=" + active +
+                '}';
+    }
+}
