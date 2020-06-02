8278ba3400914a64a620138336d35758e193026c
Espino <edespinom@gmail.com>
2020-06-02 00:16:03 -0500

    Basic Thymeleaf usage

--- src/main/java/io/maya/jobs/controller/HomeController.java
+++ src/main/java/io/maya/jobs/controller/HomeController.java
@@ -1,13 +1,18 @@
 package io.maya.jobs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String ShowHome() {
+    public String ShowHome(Model model) {
+        model.addAttribute("message", "Welcome to Jobs App!");
+        model.addAttribute("date", new Date());
         return "home";
     }
 }
