859e4339091512990005214da127cdb0c9802992
Espino <edespinom@gmail.com>
2020-06-02 00:44:55 -0500

    Thymeleaf list example

--- src/main/java/io/maya/jobs/controller/HomeController.java
+++ src/main/java/io/maya/jobs/controller/HomeController.java
@@ -5,9 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<String> jobs = new LinkedList<>();
+        jobs.add("Software Engineer");
+        jobs.add("Back-end developer");
+        model.addAttribute("jobs", jobs);
+        return "list";
+    }
 
     @GetMapping("/")
     public String showHome(Model model) {
