b053e1ad949c9d96d9f7040e6eda1c2d18fe1edd
Espino <edespinom@gmail.com>
2020-06-03 18:18:01 -0500

    @RequestMapping at method implementation

---
+++ src/main/java/io/maya/jobs/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package io.maya.jobs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CategoryController {
+
+    @GetMapping("/categories/list")
+    public String listCategories() {
+        return "categories/listCategories";
+    }
+
+    @GetMapping("/categories/create")
+    public String createCategory() {
+        return "categories/createCategories";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory() {
+        return "categories/listCategories";
+    }
+
+}
