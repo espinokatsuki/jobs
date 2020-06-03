1c6cb2f017aa1099e61b0de133b4ecb8e29fd3a9
Espino <edespinom@gmail.com>
2020-06-03 18:29:56 -0500

    RequestMapping class implementation

--- src/main/java/io/maya/jobs/controller/CategoryController.java
+++ src/main/java/io/maya/jobs/controller/CategoryController.java
@@ -3,8 +3,10 @@ package io.maya.jobs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/categories")
 public class CategoryController {
 
     @GetMapping("/list")
