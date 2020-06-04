24f6671b56b3788e8164470cb446f8c5d0bde7fe
Espino <edespinom@gmail.com>
2020-06-03 22:11:28 -0500

    @PathVariable implementation for dynamic URL

---
+++ src/main/java/io/maya/jobs/controller/VacantController.java
@@ -0,0 +1,81 @@
+package io.maya.jobs.controller;
+
+import io.maya.jobs.model.Vacant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/vacants")
+public class VacantController {
+
+    private final Logger logger = LoggerFactory.getLogger(VacantController.class);
+    private List<Vacant> vacantList = createVacantList();
+
+    @GetMapping("")
+    public String listAll(Model model) {
+        model.addAttribute("vacantList", vacantList);
+        return "vacant/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable("id") int id, Model model) {
+        // TODO: Search vacant in DDBB
+        model.addAttribute("vacant", vacantList.get(id - 1));
+        logger.info("Vacant {} called = {}", id, vacantList.get(id - 1));
+        return "vacant/detail";
+    }
+
+
+    private List<Vacant> createVacantList() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        List<Vacant> list = new LinkedList<>();
+        try {
+            Vacant vacant1 = new Vacant();
+            vacant1.setId(1);
+            vacant1.setName("Game designer");
+            vacant1.setDescription("Game designer to produce new games");
+            vacant1.setSalary(1000.0);
+            vacant1.setPublicationDate(simpleDateFormat.parse("03-06-2020"));
+            vacant1.setActive(Boolean.TRUE);
+            vacant1.setHighlighted(1);
+            vacant1.setImage("empresa1.png");
+
+            Vacant vacant2 = new Vacant();
+            vacant2.setId(2);
+            vacant2.setName("C++ game developer");
+            vacant2.setDescription("Game programer with OpenGL experience");
+            vacant2.setSalary(2000.0);
+            vacant2.setPublicationDate(simpleDateFormat.parse("02-06-2020"));
+            vacant2.setActive(Boolean.TRUE);
+            vacant2.setHighlighted(0);
+            vacant2.setImage("empresa2.png");
+
+            Vacant vacant3 = new Vacant();
+            vacant3.setId(3);
+            vacant3.setName("Character designer");
+            vacant3.setDescription("Game designer to produce new characters");
+            vacant3.setSalary(3000.0);
+            vacant3.setPublicationDate(simpleDateFormat.parse("01-06-2020"));
+            vacant3.setActive(Boolean.TRUE);
+            vacant3.setHighlighted(1);
+
+            list.add(vacant1);
+            list.add(vacant2);
+            list.add(vacant3);
+        } catch (ParseException e) {
+            logger.error(e.getMessage());
+        }
+        return list;
+    }
+
+}
